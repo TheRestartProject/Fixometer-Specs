139f07fe1c87cd70a669b5b618339e0d6934c673
Neil Mather <ngmather@gmail.com>
2017-04-18 15:28:11 +0100

    Set up codeception for automated acceptance tests

--- features/2-UserAccounts/02 UserAuthentication.feature
+++ features/2-UserAccounts/02 UserAuthentication.feature
@@ -9,17 +9,16 @@ Background:
         | fry@planetexpress.com | fry!     | 
 
 Scenario: Valid login
-    When a user logs in with email 'fry@planetexpress.com' and password 'fry!'
-    Then the user is authenticated as 'fry@planetexpress.com'
+    When a user logs in with email "fry@planetexpress.com" and password "fry!"
+    Then the user is logged in as "Fry" with email "fry@planetexpress.com"
 
 Scenario: Valid login with alternate case email
 # Emails are case-insensitive.
-    When a user logs in with email 'fry@planetexpress.com' and password 'FRY!'
-    Then the user is not logged in
-    And a message is displayed to the user letting them know they have not been logged in
+    When a user logs in with email "FRY@PlAnetExPreSs.com" and password "fry!"
+    Then the user is logged in as "Fry" with email "fry@planetexpress.com"
 
 Scenario: Invalid login due to password casing
 # Passwords are case-sensitive.
-    When a user logs in with email 'fry@planetexpress.com' and password 'FRY!'
+    When a user logs in with email "fry@planetexpress.com" and password "FRY!"
     Then the user is not logged in
     And a message is displayed to the user letting them know they have not been logged in
---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,70 @@
+<?php
+
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+
+    /**
+     * @Given the following user account has been created
+     */
+    public function theFollowingUserAccountHasBeenCreated()
+    {
+        //throw new \Codeception\Exception\Incomplete("Step `the following user account has been created` is not defined");
+    }
+
+    /**
+     * @When a user logs in with email :email and password :password
+     */
+     public function aUserLogsInWithEmailAndPassword($email, $password)
+     {
+         $this->amOnPage('/');
+         $this->submitForm('#login', [
+            'email' => $email,
+            'password' => $password,
+         ]);
+     }
+
+    /**
+     * @Then the user is logged in as :name with email :email
+     */
+    public function theUserIsAuthenticatedAs($name, $email)
+    {
+         $this->see('Welcome, ' . $name);
+    }
+
+    /**
+     * @Then the user is not logged in
+     */
+    public function theUserIsNotLoggedIn()
+    {
+         $this->seeInTitle('Login');
+    }
+
+    /**
+     * @Then a message is displayed to the user letting them know they have not been logged in
+     */
+    public function aMessageIsDisplayedToTheUserLettingThemKnowTheyHaveNotBeenLoggedIn()
+    {
+         $this->see('No user account was found for the email and password provided. Please check your details and try again.');
+    }
+}
